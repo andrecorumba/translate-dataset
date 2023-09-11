1fc51c70af90168fd41ec287beeb46df9c3586d3
Andre Luiz Monteiro da Rocha <andreluiz@MacBook-Air-de-Andre.local>
2023-09-11 16:35:26 -0400

    Azuer translate with dataset

--- scripts/azure_openai_translate.py
+++ scripts/azure_openai_translate.py
@@ -2,10 +2,50 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv
 
-from langchain.llms import AzureOpenAI
+import tqdm
+import json
+
+import datasets
+
+import time
 
 
 def azure_openai_translate():
+    # Load dataset
+    ds = my_load_dataset()
+
+    # Itarate over the dataset
+    for idx, row in enumerate(tqdm.tqdm(ds)):
+        try:
+            row['text_eng'] = '\n'.join( translate(s) for s in row['text'].split('\n'))
+            #row['text_eng'] = translate('Olá Mundo!')
+            
+            # create a new json file
+            with open(f'response/{idx:>011}.json', 'w') as f:
+                json.dump(row, f)
+        except Exception as e:
+            with open('response/error.log', 'a') as f:
+                f.write(f"{idx:>011} - {e}\n{row['text']}\n\n")
+            row['text_eng'] = row['text']
+            continue
+        
+        # Verify if the limit of 3500 requests per minute was reached
+        # if idx % 3500 == 0:
+        #     time.sleep(65)
+        if idx >= 5:
+            break
+    
+def my_load_dataset():
+    '''Load dataset'''
+    ds = datasets.load_dataset('json',
+                           data_files='/Volumes/DATA/cgu-translate-dataset/data/treinamento_v01_full.json',
+                           split='train',
+                           streaming=False)
+    return ds
+
+def translate(text):
+    '''Translate text to English'''
+
     # Use Azure environment
     _ = load_dotenv(find_dotenv())
     openai.api_key = os.environ['AZURE_OPENAI_KEY']
@@ -13,11 +53,7 @@ def azure_openai_translate():
     openai.api_type = 'azure'
     openai.api_version = '2023-05-15'
 
-    text = '''Bom dia amiguinhos já estou aqui.
-    Tenho tanta coisa para te divertir.
-    Quero ouvir você contar até três.
-    '''
-    
+    # Translate with Azure OpenAI
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         temperature=1,
@@ -25,7 +61,10 @@ def azure_openai_translate():
         messages=[{"role": "user", "content": f"Translate to English: {text}"}]
         )
 
-    print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
 
 if __name__ == '__main__':
+    print('Starting...')
     azure_openai_translate()
+    print('Done!')
